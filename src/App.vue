02e4ec8c757ab2c71d71f73a495cc54aacd339b8
Ted_Xu <1853769@tongji.edu.cn>
2022-08-22 16:28:47 +0800

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-main>
+      <baidu-map class="map" center="北京"></baidu-map>
+    </el-main>
+    <el-footer>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+    </el-footer>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
@@ -24,5 +30,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.map {
+  width: 100%;
+  height: 550px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import BaiduMap from 'vue-baidu-map'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(BaiduMap, { ak: 'Your AK' })
 
 new Vue({
   render: h => h(App),
